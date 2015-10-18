abf04d36251aacab85484582fde5e123f1bb91a9
Matthew Bullen <burner0110@gmail.com>
2015-10-17 20:59:48 -0700

    .

--- resources/views/layouts/text.blade.php
+++ resources/views/layouts/text.blade.php
@@ -24,7 +24,7 @@
             <div id="content" class="nano">
                 <div id="result" class="nano-content">
                     <div id="description">
-                        <h3 id="description-title">Using the <em>Lorem ipsum</em> Generator</h3>
+                        <h3 id="description-title">Using the <em>Lorem ipsum</em> Text Generator</h3>
                         <div id="description-content">
                             <p class="description-content-paragraph">Use the slider above to select the number of paragraphs to generate. The generator defaults to three paragraphs, but you can select up to twenty.</p>
                             <p class="description-content-paragraph">Then click the "Get lorem ipsum text!" button, and the new text content will be displayed in this panel. The text is left-click selectable and will mouse scroll for easier copy/paste.</p>
